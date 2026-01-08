0ac2e5d871c5cafaef8ccd99c9d25a3b7a638962
Amos Asunda <kuhlali032@gmail.com>
2026-01-08 11:03:42 +0300

    Delete extra build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r chamapro/requirements.txt
-
-python chamapro/manage.py collectstatic --no-input
-python chamapro/manage.py migrate
